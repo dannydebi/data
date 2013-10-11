1250f9af10f8ec4f7cb9d795a5d42639454713fd
Sebastiaan van der Vliet <developer@innerballoons.com>
2013-10-11 13:43:43 +0200

    Update austrian_profiles.jl

--- austrian_profiles.jl
+++ austrian_profiles.jl
@@ -2,3 +2,4 @@
 {"business_profile":{"address":"HStephansplatz 12","business_name":"Aioli Tapas & Antipasti","city":"Wien","id":"","main_phone":"01 5320373","website":"","zip_code":"1010","geo":[52.3396,4.96247]}}
 {"business_profile":{"address":"Schloßpark Schönbrunn","business_name":"Tiroler Garten","city":"Wien","id":"","main_phone":"01 5320373","website":"","zip_code":"1030","geo":[52.3396,4.96247]}}
 {"business_profile":{"address":"Kärntnerstraße 47","business_name":"Opera Suites","city":"Wien","id":"","main_phone":"01 5320373","website":"","zip_code":"1010","geo":[52.3396,4.96247]}}
+{"business_profile":{"address":"Sulz 86A","business_name":"CW Auto-Diskont Ersatzteile","city":"Gerersdof","id":"","main_phone":"01 5320373","website":"","zip_code":"7542","geo":[52.3396,4.96247]}}
