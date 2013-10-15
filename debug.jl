64a63cdfa1c8bb6398ceab102785e243971139d0
Sebastiaan van der Vliet <developer@innerballoons.com>
2013-10-15 16:40:47 +0200

    Update debug.jl

--- debug.jl
+++ debug.jl
@@ -1,1 +1,1 @@
-{"advert_id":"10210937","url":"http://www.yell.com/biz/convenient-hire-837623","spider":"yell-uk"}
+{"advert_id":"10210937","url":"http://www.yell.com/biz/convenient-hire-837623/","spider":"yell-uk"}
