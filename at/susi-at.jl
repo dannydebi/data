3ea6b60e88022a7ee6e3cbdb31f3c39d3b53f0b0
Sebastiaan van der Vliet <developer@innerballoons.com>
2013-12-13 12:34:21 +0100

    Update susi-at.jl

--- at/susi-at.jl
+++ at/susi-at.jl
@@ -1,1 +1,1 @@
-{"business_profile": {"business_name": "Neundlinger Schuhmoden GmbH", "geo": "48.4678200501535,14.1633297781628", "city": "Sankt Veit im Mühlkreis", "street_number": "15-16", "zip_code": "4173"}}
+{"business_profile": {"business_name": "Neundlinger Schuhmoden GmbH", "geo": [], "city": "Sankt Veit im Mühlkreis", "street_number": "15-16", "zip_code": "4173"}}
