e0ffbdc2dea04086822d345acfad5b9b93d3619a
Sebastiaan van der Vliet <developer@innerballoons.com>
2013-12-18 09:47:50 +0100

    Update facebook-at.jl

--- at/facebook-at.jl
+++ at/facebook-at.jl
@@ -1,2 +1,1 @@
-{"business_profile": {"business_name": "Pension Vogelhändler", "geo": "47.6666718293642,14.3459351291833", "city": "Spital am Pyhrn", "street_number": "1", "zip_code": "4582"}}
-{"business_profile": {"business_name": "Vakutec Gülletechnik GmbH", "geo": "47.9810793626022,14.1288381894457", "city": "Nußbach", "street_number": "14", "zip_code": "4542"}}
+ {"business_profile": {"business_name": "ADEG Bader", "geo": "47.8309721920404,13.548568548839", "city": "Steinbach am Attersee", "street_number": "8", "zip_code": "4853"}}
